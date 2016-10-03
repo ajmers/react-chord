fb8ce3f0357b3d1342614fee640f7bab987d1db5
Anne Maiale <amaiale@gmail.com>
2016-10-03 18:11:29 -0400

    add styles as inline objects

--- src/chord.js
+++ src/chord.js
@@ -21,13 +21,16 @@ export default class Chord extends Component {
         let minFretEl;
         if (isEditable && onMinFretChange) {
             minFretEl = (<Input
+                style={chordMinFretInputStyle}
                 className='chord__min-fret-input'
                 value={minFret || ''}
                 onChange={onMinFretChange}
                 >fr</Input>);
         } else {
             minFretEl = (
-                <span className='chord__min-fret'>
+                <span
+                    style={chordMinFretStyle}
+                    className='chord__min-fret'>
                     {minFret ? minFret + 'fr' : ''}
                 </span>
             );
@@ -39,7 +42,8 @@ export default class Chord extends Component {
         const { chord, numFrets } = this.props;
         const { minFret } = chord;
         return (
-            <div className='chord-chart'>
+            <div className='chord-chart'
+                style={chordChartStyle}>
                 {this.renderMinFret(minFret)}
                 <div className='chord-chart__strings'>
                     {chord.fingerings.map((string, i) => {
@@ -55,3 +59,28 @@ export default class Chord extends Component {
         );
     }
 }
+
+const chordMinFretStyle = {
+    fontSize: '.75em',
+    position: 'absolute',
+    left: 20,
+};
+
+const chordMinFretInputStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    position: 'absolute',
+    fontSize: '.75rem',
+    left: 20,
+    top: 30,
+    width: 20,
+    padding: 0,
+};
+
+const chordChartStyle = {
+    display: 'flex',
+    position: 'relative',
+    alignSelf: 'center',
+    padding: 30,
+};
